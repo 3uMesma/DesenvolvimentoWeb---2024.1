a6c478f17054fad2d00acae228f139a0bf535cbc
Alcino Salviano Cavalcanti <alcino.cavalcanti@usp.br>
2024-04-26 10:56:48 -0300

    addHome.js
    adicionei um arquivo .jsx para pagina da home

---
+++ front-astro-cariri/src/components/data/home.jsx
@@ -0,0 +1,9 @@
+import about_image from '../../images/astronaut-image.jpg';
+
+export const fakeHome = [
+    {
+        citacao: "“Somos feitos de poeira de estrelas. Nós somos uma maneira de o cosmos se autoconhecer.” - Carl Sagan",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate eu nunc sit amet commodo. Proin at augue eget odio elementum fringilla sed at tortor. Phasellus in diam et massa interdum porta sed a neque. Fusce vel nibh sollicitudin, tincidunt nibh vel, accumsan velit. Morbi eleifend sapien non odio ullamcorper accumsan. Nullam gravida orci ac nisl consectetur, eu accumsan tellus lacinia. Proin fermentum leo lectus, sed consequat nisl tincidunt vitae.",
+        imagem_descricao: about_image
+    }
+];
--- front-astro-cariri/src/components/pages/home/Home.js
+++ front-astro-cariri/src/components/pages/home/Home.js
@@ -1,9 +1,11 @@
 import './Home.css'
 import Header from '../../layout/header/Navbar.js'
 import GlobalStyle from '../../../styles/GlobalStyle.js';
-import gato_logo from '../../../images/astronaut-image.jpg';
+import { fakeHome } from '../../data/home.jsx';
 
-function Home(){
+function Home(props){
+    const home = fakeHome[0];
+
     return(
         <div className='home'>
             <GlobalStyle/>
@@ -11,15 +13,15 @@ function Home(){
                 <Header/>
             </div>
             <div className='home-main'>
-                <h1 className='home-main-title'>“Somos feitos de poeira de estrelas. Nós somos uma maneira de o cosmos se autoconhecer.” - Carl Sagan</h1>
+                <h1 className='home-main-title'>{home.citacao}</h1>
             </div>
             <div className='home-about'>
                 <div className='home-about-left'>
                     <h1 className='home-about-title'>Quem Somos?</h1>
-                    <p className='home-about-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate eu nunc sit amet commodo. Proin at augue eget odio elementum fringilla sed at tortor. Phasellus in diam et massa interdum porta sed a neque. Fusce vel nibh sollicitudin, tincidunt nibh vel, accumsan velit. Morbi eleifend sapien non odio ullamcorper accumsan. Nullam gravida orci ac nisl consectetur, eu accumsan tellus lacinia. Proin fermentum leo lectus, sed consequat nisl tincidunt vitae. </p>
+                    <p className='home-about-description'>{home.descricao}</p>
                 </div>
                 <div className='home-about-right'>
-                    <img src={gato_logo} alt="astronaut" className="home-about-image"></img>
+                    <img src={home.imagem_descricao} alt="astronaut" className="home-about-image"></img>
                 </div>
             </div>
         </div>
