a1e2301fccc2a75817b987ff2d91a5f6a984fff4
3uMesma <kasatamanda@gmail.com>
2024-05-03 11:05:38 -0300

    Feat: mudanças na home admin propostas pela Letícia

--- front-astro-cariri/src/components/pages/homeAdmin/HomeAdmin.js
+++ front-astro-cariri/src/components/pages/homeAdmin/HomeAdmin.js
@@ -2,6 +2,8 @@ import './HomeAdmin.css'
 import Header from '../../layout/header-admin/Navbar.js'
 import GlobalStyle from '../../../styles/GlobalStyle.js';
 
+import eventos from "../../data/eventos.json"
+
 function HomeAdmin(){
     return(
         <div className='homeAdmin'>
@@ -13,42 +15,27 @@ function HomeAdmin(){
                 <div className="homeAdmin-title">
                     <h1 className='homeAdmin-title'>Últimos Pedidos</h1>
                 </div>
-                <div className='homeAdmin-proposals'>
-                    <div className='homeAdmin-proposals-proposal'>
-                        <h2 className='homeAdmin-proposal-title'>PROPOSTA1</h2>
-                        <p className='p-proposal'><strong>Interessado: </strong> Interessado1</p>
-                        <p className='p-proposal'><strong>Instituição: </strong> Instituição1</p>
-                        <p className='p-proposal'><strong>Data: </strong> 01/01/0001, 01h01</p>
-                        <p className='p-proposal'><strong>Tipo: </strong> Minicurso</p>
-                    </div>
-                    <div className='homeAdmin-proposals-proposal'>
-                        <h2 className='homeAdmin-proposal-title'>PROPOSTA2</h2>
-                        <p className='p-proposal'><strong>Interessado: </strong> Interessado2</p>
-                        <p className='p-proposal'><strong>Instituição: </strong> Instituição2</p>
-                        <p className='p-proposal'><strong>Data: </strong> 02/02/0002, 02h02</p>
-                        <p className='p-proposal'><strong>Tipo: </strong> Oficina</p>
-                    </div>
-                    <div className='homeAdmin-proposals-proposal'>
-                        <h2 className='homeAdmin-proposal-title'>PROPOSTA3</h2>
-                        <p className='p-proposal'><strong>Interessado: </strong> Interessado3</p>
-                        <p className='p-proposal'><strong>Instituição: </strong> Instituição3 Instituição3 Instituição3 Instituição3 Instituição3</p>
-                        <p className='p-proposal'><strong>Data: </strong> 03/03/0003, 03h03</p>
-                        <p className='p-proposal'><strong>Tipo: </strong> Oficina</p>
-                    </div>
-                    <div className='homeAdmin-proposals-proposal'>
-                        <h2 className='homeAdmin-proposal-title'>PROPOSTA4</h2>
-                        <p className='p-proposal'><strong>Interessado: </strong> Interessado4</p>
-                        <p className='p-proposal'><strong>Instituição: </strong> Instituição4</p>
-                        <p className='p-proposal'><strong>Data: </strong> 04/04/0004, 04h04</p>
-                        <p className='p-proposal'><strong>Tipo: </strong> Oficina</p>
-                    </div>
-                    <div className='homeAdmin-proposals-proposal'>
+                <div className=' proposals-container'>
+                <ul className='homeAdmin-proposals'>
+                    {eventos.map((proposta, index) => (
+                    <li key={index}>
+                        <div className='homeAdmin-proposals-proposal'>
+                            <h2 className='homeAdmin-proposal-title'>{proposta.titulo}</h2>
+                            <p className='p-proposal'><strong>Interessado: </strong> {proposta['nome-interessado']}</p>
+                            <p className='p-proposal'><strong>Instituição: </strong> {proposta.instituicao}</p>
+                            <p className='p-proposal'><strong>Data: </strong> {proposta.data}</p>
+                            <p className='p-proposal'><strong>Tipo: </strong> {proposta.tipo}</p>
+                        </div>
+                    </li>
+                    ))}
+                </ul>
+                    {/* <div className='homeAdmin-proposals-proposal'>
                         <h2 className='homeAdmin-proposal-title'>PROPOSTA5</h2>
                         <p className='p-proposal'><strong>Interessado: </strong> Interessado5</p>
                         <p className='p-proposal'><strong>Instituição: </strong> Instituição5</p>
                         <p className='p-proposal'><strong>Data: </strong> 05/05/0005, 05h05</p>
                         <p className='p-proposal'><strong>Tipo: </strong> Oficina</p>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
