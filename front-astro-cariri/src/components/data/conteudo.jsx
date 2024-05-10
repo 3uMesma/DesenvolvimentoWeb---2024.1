05b4fa222d6b04d112be3f256d6347155afdf87d
Lucas Issao Omati <lucasomatioficial@gmail.com>
2024-05-10 19:03:34 -0300

    add .jsx archive on page EditarMaterial

---
+++ front-astro-cariri/src/components/data/conteudo.jsx
@@ -0,0 +1,9 @@
+export const fakeConteudo = [
+  {
+    titulo: "Visão da Astronomia Antiga",
+    autor: "Fulano",
+    topico: "Histórico",
+    texto: "A astronomia antiga baseava-se na observação dos padrões regulares dos movimentos de objetos celestiais visíveis, especialmente o Sol, a Lua, estrelas, e os planetas vistos à olho nu.",
+    legenda: "Imagem de Platão"
+}
+];
