4e83c6e3ed0403afd32b30711aa4c5b73791bb9c
3uMesma <kasatamanda@gmail.com>
2024-05-10 10:45:39 -0300

    Hotfix: ajustes na tela home admin

--- front-astro-cariri/src/components/pages/homeAdmin/HomeAdmin.js
+++ front-astro-cariri/src/components/pages/homeAdmin/HomeAdmin.js
@@ -11,31 +11,24 @@ function HomeAdmin(){
             <div className='header-style'>
                 <Header/>
             </div>
-            <div className='body'>
+            <div className='home-admin-body'>
                 <div className="homeAdmin-title">
                     <h1 className='homeAdmin-title'>Últimos Pedidos</h1>
                 </div>
                 <div className=' proposals-container'>
-                <ul className='homeAdmin-proposals'>
-                    {eventos.map((proposta, index) => (
-                    <li key={index}>
-                        <div className='homeAdmin-proposals-proposal'>
-                            <h2 className='homeAdmin-proposal-title'>{proposta.titulo}</h2>
-                            <p className='p-proposal'><strong>Interessado: </strong> {proposta['nome-interessado']}</p>
-                            <p className='p-proposal'><strong>Instituição: </strong> {proposta.instituicao}</p>
-                            <p className='p-proposal'><strong>Data: </strong> {proposta.data}</p>
-                            <p className='p-proposal'><strong>Tipo: </strong> {proposta.tipo}</p>
-                        </div>
-                    </li>
-                    ))}
-                </ul>
-                    {/* <div className='homeAdmin-proposals-proposal'>
-                        <h2 className='homeAdmin-proposal-title'>PROPOSTA5</h2>
-                        <p className='p-proposal'><strong>Interessado: </strong> Interessado5</p>
-                        <p className='p-proposal'><strong>Instituição: </strong> Instituição5</p>
-                        <p className='p-proposal'><strong>Data: </strong> 05/05/0005, 05h05</p>
-                        <p className='p-proposal'><strong>Tipo: </strong> Oficina</p>
-                    </div> */}
+                    <ul className='homeAdmin-proposals'>
+                        {eventos.map((proposta, index) => (
+                        <li key={index}>
+                            <div className='homeAdmin-proposals-proposal'>
+                                <h2 className='homeAdmin-proposal-title'>{proposta.titulo}</h2>
+                                <p className='p-proposal'><strong>Interessado: </strong> {proposta['nome-interessado']}</p>
+                                <p className='p-proposal'><strong>Instituição: </strong> {proposta.instituicao}</p>
+                                <p className='p-proposal'><strong>Data: </strong> {proposta.data}</p>
+                                <p className='p-proposal'><strong>Tipo: </strong> {proposta.tipo}</p>
+                            </div>
+                        </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         </div>
