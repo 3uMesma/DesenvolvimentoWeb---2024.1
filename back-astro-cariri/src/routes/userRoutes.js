6e882d354f94b80b3fe7dae6fe8bd7d2cc0b1858
Lucas Issao Omati <lucasomatioficial@gmail.com>
2024-06-18 00:31:59 -0300

    Feat: DELETE user

--- back-astro-cariri/src/routes/userRoutes.js
+++ back-astro-cariri/src/routes/userRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const getUserController = require('../controllers/user/get');
 const putUserController = require('../controllers/user/put');
 const postUserController = require('../controllers/user/post');
+const deleteUserController = require('../controllers/user/delete');
 
 router.use(express.json());
 
 router.get('/user/:userId', getUserController.getUserData);
 router.put('/user/:userId', putUserController.putUserData);
 router.post('/user/', postUserController.postNewUser);
+router.delete('/user/:userId', deleteUserController.deleteUser);
 
 module.exports = router;
